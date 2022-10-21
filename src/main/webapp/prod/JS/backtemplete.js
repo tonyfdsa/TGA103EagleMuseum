73902065ced96494a78f927672ca5cfb4b8e7d09
tonyfdsa <tonyfdsa@gmail.com>
2022-10-21 16:00:19 +0800

    路徑更新

---
+++ src/main/webapp/prod/JS/backtemplete.js
@@ -0,0 +1,11 @@
+$(".sideBtn").click(function () {
+    $(this).nextAll(".Btn").slideToggle("slow");
+  });
+
+  window.addEventListener("load", function() { 
+
+    if(sessionStorage.getItem('memberName') != null){
+      document.querySelector('#memberName').textContent = 
+      sessionStorage.getItem('memberName') + "，您好！";
+    }
+  });
